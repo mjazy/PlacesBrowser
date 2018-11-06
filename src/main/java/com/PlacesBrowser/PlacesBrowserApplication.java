00368ec6d698c879004f862555199eb4b5e4d22c
mjazy <112projects.contact@gmail.com.com>
2018-11-06 20:06:24 +0100

    Initial commit: project created

---
+++ src/main/java/com/PlacesBrowser/PlacesBrowserApplication.java
@@ -0,0 +1,12 @@
+package com.PlacesBrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesBrowserApplication.class, args);
+	}
+}
