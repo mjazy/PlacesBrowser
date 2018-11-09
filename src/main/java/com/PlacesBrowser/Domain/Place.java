b0439f48175e03ac8d32537cf557e9674265d198
mjazy <112projects.contact@gmail.com.com>
2018-11-09 20:16:55 +0100

    6th commit: Place data model added

---
+++ src/main/java/com/PlacesBrowser/Domain/Place.java
@@ -0,0 +1,55 @@
+package com.PlacesBrowser.Domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Data model for place in format compliant with requirements.
+ * @author MJazy
+ *
+ */
+public class Place {
+
+	@JsonProperty
+	private String name;
+	
+	@JsonProperty
+	private float latitude;
+	
+	@JsonProperty
+	private float longitude;
+	
+	public Place() {
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}	
+
+	public float getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	
+	public float getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	
+	@Override
+	public String toString() {
+		String string = String.format("name: '%s', latitude: '%s', longitude: '%s'", this.getName(), this.getLatitude(), this.getLongitude());
+		return string;
+	}
+	
+	
+}
